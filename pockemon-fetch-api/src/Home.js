2edfcf2f7496f4bda0f4f4fd683b4dc8d5ecba46
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2023-08-24 12:08:17 -0700

    fetch api

--- pockemon-fetch-api/src/Home.js
+++ pockemon-fetch-api/src/Home.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
     const [num,setNum] = useState();
+    const [name, setName] = useState();
+    const [move, setMove] = useState();
+
+    useEffect(() => {
+        async function getData() {
+            const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+            console.log(res.data.name);
+            setName(res.data.name);
+            setMove(res.data.moves.length);
+        }
+
+        getData();
+    });
+
   return (
     <>
-    <h1>You have select {num} value...</h1>
-    
+    <h1>You have select <span style={{color:'red'}}>{num}</span> value...</h1>
+    <h1>My name is <span style={{color:'red'}}> {name} </span> </h1>
+    <h1>I have <span style={{color:'red'}}>{move}</span> </h1>
+
       <select value={num} onChange={ (event) => {
         setNum (event.target.value);
       }}>
